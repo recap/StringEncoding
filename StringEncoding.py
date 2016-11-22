cd8528bf4cd575f3dffefd64cbb34c3c2eaa8945
Reggie <cushingreggie@gmail.com>
2016-11-22 13:56:22 +0100

    updates

--- StringEncoding.py
+++ StringEncoding.py
@@ -8,20 +8,24 @@ class StringEncoding:
 	def decode(self, s):
 		decoded_string = ""
 		i = 0
-		for i in range(0, len(s), 2):
-			if(s[i] == 0x00):
-				# check valid ascii char
-				if(s[i+1] > 32) and (s[i+1] < 127):
-					decoded_string += chr(s[i+1])
+		try:
+			for i in range(0, len(s), 2):
+				if(s[i] == 0x00):
+					# check valid ascii chars
+					if(s[i+1] > 32) and (s[i+1] < 127):
+						decoded_string += chr(s[i+1])
+					else:
+						decoded_string += "\3F"
 				else:
-					decoded_string += "\3F"
-			else:
-				start_index = len(decoded_string) - s[i]
-				end_index = start_index + s[i+1]
-				if((s[i] > len(decoded_string)) or (end_index > len(decoded_string))):
-					decoded_string += "\3F"
-				else:
-					decoded_string += decoded_string[start_index:end_index]
+					#check if indecies are within bounds
+					start_index = len(decoded_string) - s[i]
+					end_index = start_index + s[i+1]
+					if((s[i] > len(decoded_string)) or (end_index > len(decoded_string))):
+						decoded_string += "\3F"
+					else:
+						decoded_string += decoded_string[start_index:end_index]
+		except:
+			decoded_string = None
 
 		return decoded_string
 
@@ -34,29 +38,31 @@ class StringEncoding:
 		byte_stream = []
 		repeat_dict = {}
 		position_dict = {}
-		
-		if(trivial):
-			for c in s:
-				byte_stream.append(0x0)
-				byte_stream.append(ord(c))
+	
+		try:	
+			if(trivial):
+				for c in s:
+					byte_stream.append(0x0)
+					byte_stream.append(ord(c))
 
-		else:
-			# ran out of time. the basic idea is to get the repetitions which I do here. Then start from the longest
-			# repetition and substitute the repetitions excpet the first one with indecies. Next check if the smaller
-			# repetitions are substrings of the larger repetition in which case ignore, else do the same procedure of
-			# substitution.	
-			byte_stream = list(self.repetitions(s))
-			#for r in list(repetitions(l)):
-			#	a = [m.start() for m in re.finditer(r[0], l)]
-			#	#repeat_dict[r[0]] = (a[0], len[r[0]])				
-			#	repeat_dict[r[0]] = a[0]
-			#	a.pop(0)
-			#	for e in a:
-			#		position_dict[e] = r[0]
+			else:
+				# ran out of time. the basic idea is to get the repetitions which I do here. Then start from the longest
+				# repetition and substitute the repetitions excpet the first one with indecies. Next check if the smaller
+				# repetitions are substrings of the larger repetition in which case ignore, else do the same procedure of
+				# substitution.	
+				byte_stream = list(self.repetitions(s))
+				#for r in list(repetitions(l)):
+				#	a = [m.start() for m in re.finditer(r[0], l)]
+				#	#repeat_dict[r[0]] = (a[0], len[r[0]])				
+				#	repeat_dict[r[0]] = a[0]
+				#	a.pop(0)
+				#	for e in a:
+				#		position_dict[e] = r[0]
 
-			#print repeat_dict
-			#print position_dict
-			
+				#print repeat_dict
+				#print position_dict
+		except:
+			byte_stream = None	
 
 		return byte_stream
 
--- run.py
+++ run.py
@@ -12,13 +12,19 @@ bytes = sys.stdin.read()
 x = bytes[:-1].decode("hex")
 d = bytearray(x)
 l = myEncoding.decode(d)
-k = os.getenv("USE_TRIVIAL_IMPLEMENTATION", 0)
-if(int(k) ==  1):
-	r = myEncoding.encode(l,True)
-else:
-	r = myEncoding.encode(l,False)
+if(l):
+	k = os.getenv("USE_TRIVIAL_IMPLEMENTATION", 0)
+	if(int(k) ==  1):
+		r = myEncoding.encode(l,True)
+	else:
+		r = myEncoding.encode(l,False)
+
 	
 
 sys.stdout.write("stdout: " +l+"\n")
-sys.stderr.write("stderr: "+str(r)+"\n")
+
+if(r):
+	sys.stderr.write("stderr: "+str(r)+"\n")
+else:
+	sys.stderr.write("stderr: Error\n");
 
